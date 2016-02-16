318cf9e6a99d826a265cff8c784a87409881f5b9
Orry Baram <orry@mic.com>
2016-02-16 15:26:48 -0500

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+/**
+ * idleTimer
+ *
+ * If user is idle for idleTime fire callback
+ * 
+ * @param {object} options
+ *    - {function} callback - fired when user is idle
+ *    - {Number} idleTime - time in milliseconds  
+ */
+
+module.exports = idleTimer;
+
+function idleTimer(options) {
+  options = options || {};
+  var callback = options.callback || function() {};
+  var idleTime = options.idleTime || 60000;
+  var timer;
+  
+  window.onload = resetTimer;
+  document.onmousemove = resetTimer;
+  document.onkeypress = resetTimer;
+
+  function resetTimer() {
+    clearTimeout(timer);
+    timer = setTimeout(callback, idleTime);
+  }
+}
